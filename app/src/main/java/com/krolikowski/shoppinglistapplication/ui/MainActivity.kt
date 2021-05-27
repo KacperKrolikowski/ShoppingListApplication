7bd4007b078a14032b7103a980e05a7705b2ea9a
Kacper Królikowski <kaacperkrolikowski@gmail.com>
2021-05-27 12:32:24 +0200

    Database instane, DAO

--- app/src/main/java/com/krolikowski/shoppinglistapplication/ui/MainActivity.kt
+++ app/src/main/java/com/krolikowski/shoppinglistapplication/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.krolikowski.shoppinglistapplication
+package com.krolikowski.shoppinglistapplication.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.krolikowski.shoppinglistapplication.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
