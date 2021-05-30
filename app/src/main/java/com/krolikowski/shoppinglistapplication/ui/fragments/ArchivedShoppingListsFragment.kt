b630af6732f2f36c147cc32ab9ef1de22fbc3b54
Kacper Królikowski <kaacperkrolikowski@gmail.com>
2021-05-30 10:38:45 +0200

    Working on Styles

--- app/src/main/java/com/krolikowski/shoppinglistapplication/ui/fragments/ArchivedShoppingListsFragment.kt
+++ app/src/main/java/com/krolikowski/shoppinglistapplication/ui/fragments/ArchivedShoppingListsFragment.kt
@@ -3,7 +3,6 @@ package com.krolikowski.shoppinglistapplication.ui.fragments
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.krolikowski.shoppinglistapplication.R
@@ -12,9 +11,9 @@ import com.krolikowski.shoppinglistapplication.data.db.ShoppingDatabase
 import com.krolikowski.shoppinglistapplication.data.repositories.ShoppingRepository
 import com.krolikowski.shoppinglistapplication.ui.viewmodels.ShoppingViewModel
 import com.krolikowski.shoppinglistapplication.ui.viewmodels.ShoppingViewModelFactory
-import kotlinx.android.synthetic.main.fragmeny_archived_shopping_lists.*
+import kotlinx.android.synthetic.main.fragment_archived_shopping_lists.*
 
-class ArchivedShoppingListsFragment : Fragment(R.layout.fragmeny_archived_shopping_lists){
+class ArchivedShoppingListsFragment : Fragment(R.layout.fragment_archived_shopping_lists){
 
     lateinit var viewModel: ShoppingViewModel
     lateinit var listsAdapter: ShoppingListsAdapter
